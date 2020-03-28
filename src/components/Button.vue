9a20f0240db979eae47a3183531116267e7c8dc2
Ami Nakano <ami.nakano@outlook.com>
2020-03-28 01:00:28 +0100

    set typography

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -12,19 +12,19 @@ export default {
 </script>
 
 <style scoped lang="scss">
-button {
-    height: 3rem;
-    border-radius: 3px;
-    background: #6072e1;
-    color: #fff;
-    border: none;
-    outline: none;
-    font-size: 1.25rem;
-    opacity: 1;
+// button {
+//     height: 3rem;
+//     border-radius: 3px;
+//     background: #6072e1;
+//     color: #fff;
+//     border: none;
+//     outline: none;
+//     font-size: 1.25rem;
+//     opacity: 1;
 
-    &:hover {
-        cursor: pointer;
-        opacity: 0.8;
-    }
-}
+//     &:hover {
+//         cursor: pointer;
+//         opacity: 0.8;
+//     }
+// }
 </style>
--- src/views/JobDetail.vue
+++ src/views/JobDetail.vue
@@ -2,9 +2,10 @@
   <div class="job-detail">
     <Spinner />
     <div class="banner">
-      <router-link to="/" class="link"><i class="fas fa-arrow-left"></i> Go back</router-link>
+      <router-link to="/" class="link"><i class="fas fa-arrow-left"></i> Go back</router-link>      
       <div class="img-container">
         <img class="logo" v-bind:src="viewDetail.company_logo"/>
+        <a class="btn outline" v-bind:href="viewDetail.company_url" target="_blank"><Button msg="Company website"/></a>
       </div>
       <h1>{{ viewDetail.title }}</h1>
       <h2>{{ viewDetail.company }}</h2>
@@ -13,13 +14,13 @@
         <p>{{ viewDetail.location }} | </p>
         <p>Posted: {{ getDate() }}</p>
       </div>
-      <a class="apply-btn" v-bind:href="viewDetail.url" target="_blank"><Button msg="Apply for this job"/></a>
+      <a class="btn apply" v-bind:href="viewDetail.url" target="_blank"><Button msg="Apply for this job"/></a>
 
     </div>
     <div class="job-desc">
       <p class="job-desc-detail" v-html="viewDetail.description"></p>
-      <a v-bind:href="viewDetail.url" target="_blank"><Button msg="Apply for this job"/></a>
-      <router-link to="/" class="link">Go back</router-link>
+      <a class="btn apply" v-bind:href="viewDetail.url" target="_blank"><Button msg="Apply for this job"/></a>
+      <router-link to="/" class="link"><i class="fas fa-arrow-left"></i> Go back</router-link>
     </div>
       
   </div>
@@ -82,62 +83,55 @@ export default {
   margin-top: 3rem;
 
   .link {
-        color: #6072e1;
-        align-self: flex-end;
-      }
+    margin: 1rem 0;
+    align-self: flex-end;
+  }
 
-    .banner {
-      padding: 0 5% 1.5rem;
+  .banner {
+    padding: 0 5% 1.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    @media screen and (min-width: 768px) {
+      padding: 0 15% 1.5rem;
+    }
+
+    h1 {
+      text-align: left;
+    }
+    h2 {
+      text-align: left;
+      margin-bottom: 0.5rem;
+    }
+    .img-container {
       display: flex;
-      flex-direction: column;
+      padding: 1rem 0;
       justify-content: space-between;
-      @media screen and (min-width: 768px) {
-        padding: 0 15% 1.5rem;
-      }
-
-      h1 {
-        font-size: 2.5rem;
-        text-align: left;
-      }
-      h2 {
-        text-align: left;
-        margin-bottom: 0.5rem;
-      }
-      .img-container {
-        display: flex;
-        padding: 1rem 0;
-        .logo {
-          max-height: 100px;
-          max-width: 180px;
-        }
-      }
-
-      .info {
-        display: flex;
-          p {
-            margin-right: 0.5rem;
-          }
-      }
-
-      
-
-      .apply-btn {
-        align-self: flex-end;
-        width: 12rem;
+      .logo {
+        max-height: 100px;
+        max-width: 180px;
       }
     }
-    .job-desc {
-      background: #e0dede;
+
+    .info {
+      display: flex;
+        p {
+          margin-right: 0.5rem;
+        }
+    }
+  }
+  .job-desc {
+      background: rgba(96,114,225,0.1);
       padding: 1rem 5%;
       display: flex;
       flex-direction: column;
 
       @media screen and (min-width: 768px) {
-        padding: 1rem 20%;
+        padding: 1rem 22%;
       }
       .job-desc-detail {
-        font-size: 1.25rem;
-        margin: 1.5rem 0 3rem;
+        font-size: 1.1rem;
+        margin: 4rem 0 3rem;
 
         a {
           color: #6072e1;
@@ -154,15 +148,15 @@ export default {
           }
         }
       }
-      
-      // .link {
-      //   align-self: flex-end;
-      // }
-  }
 
-  Button {
-    align-self: flex-end;
-    width: 12rem;
+  }
+  .btn {
+    &.outline {
+      align-self: flex-end;
+    }
+    &.apply {
+      align-self: center;
+    }
   }
 }
 
